e85f8f017974fbb4ba55d539aa8120dfce473a0f
Sebastiaan Arendsen <sebastiaan@tetra.nl>
2022-08-24 13:32:10 +0200

    Updated lspsaga settings

--- nvim/.config/nvim/lua/lsp-saga.lua
+++ nvim/.config/nvim/lua/lsp-saga.lua
@@ -1,18 +1,20 @@
+local saga = require('lspsaga')
 local action = require("lspsaga.codeaction")
-local term = require("lspsaga.floaterm")
+
+saga.init_lsp_saga()
 
 -- lsp finder to find the cursor word definition and reference
-vim.keymap.set("n", "gh", require("lspsaga.finder").lsp_finder, { silent = true,noremap = true })
+vim.keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true,noremap = true })
 
 -- code action
-vim.keymap.set("n", "<leader>ca", action.code_action, { silent = true,noremap = true })
-vim.keymap.set("v", "<leader>ca", function()
-    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-U>", true, false, true))
-    action.range_code_action()
-end, { silent = true,noremap =true })
+vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true,noremap = true })
+vim.keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", { silent = true,noremap =true })
+
+-- rename
+vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>", { silent = true, noremap = true })
 
 -- show hover doc and press twice will jumpto hover window
-vim.keymap.set("n", "K", require("lspsaga.hover").render_hover_doc, { silent = true })
+vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 
 -- scroll down hover doc or scroll in definition preview
 vim.keymap.set("n", "<C-f>", function()
@@ -23,8 +25,6 @@ vim.keymap.set("n", "<C-b>", function()
     action.smart_scroll_with_saga(-1)
 end, { silent = true })
 
--- rename
-vim.keymap.set("n", "gr", require("lspsaga.rename").lsp_rename, { silent = true, noremap = true })
 
 -- line diagnostic
 vim.keymap.set("n", "<leader>cd", require("lspsaga.diagnostic").show_line_diagnostics, { silent = true,noremap = true })
