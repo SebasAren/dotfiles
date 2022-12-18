2d0588190b0125c0681852d7e5d925a59ceb8912
Sebastiaan <sebastiaan@arendsen.dev>
2022-12-18 13:40:13 +0100

    Stylua on settings.lua and removed mini.starter

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -1,14 +1,14 @@
 HOME = os.getenv("HOME")
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 vim.opt.termguicolors = true
 
 -- basic settings
 vim.o.encoding = "utf-8"
 vim.o.backspace = "indent,eol,start" -- backspace works on every char in insert mode
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 vim.o.history = 1000
-vim.o.dictionary = '/usr/share/dict/words'
+vim.o.dictionary = "/usr/share/dict/words"
 vim.o.startofline = true
 
 -- Mapping waiting time
@@ -17,7 +17,7 @@ vim.o.ttimeout = true
 vim.o.ttimeoutlen = 100
 
 -- Display
-vim.o.showmatch  = true -- show matching brackets
+vim.o.showmatch = true -- show matching brackets
 vim.o.scrolloff = 3 -- always show 3 rows from edge of the screen
 vim.o.synmaxcol = 300 -- stop syntax highlight after x lines for performance
 vim.o.laststatus = 2 -- always show status line
@@ -25,15 +25,15 @@ vim.o.laststatus = 2 -- always show status line
 vim.o.list = false -- do not display white characters
 vim.o.foldenable = false
 vim.o.foldlevel = 4 -- limit folding to 4 levels
-vim.o.foldmethod = 'syntax' -- use language syntax to generate folds
+vim.o.foldmethod = "syntax" -- use language syntax to generate folds
 vim.o.wrap = false --do not wrap lines even if very long
 vim.o.eol = false -- show if there's no eol char
-vim.o.showbreak= '↪' -- character to show when line is broken
+vim.o.showbreak = "↪" -- character to show when line is broken
 
 -- Sidebar
 vim.o.number = true -- line number on the left
 vim.o.numberwidth = 3 -- always reserve 3 spaces for line number
-vim.o.signcolumn = 'yes' -- keep 1 column for coc.vim  check
+vim.o.signcolumn = "yes" -- keep 1 column for coc.vim  check
 vim.o.modelines = 0
 vim.o.showcmd = true -- display command in bottom bar
 
@@ -49,16 +49,16 @@ vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.tabstop = 2 -- 1 tab = 2 spaces
 vim.o.shiftwidth = 2 -- indentation rule
-vim.o.formatoptions = 'qnj1' -- q  - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
+vim.o.formatoptions = "qnj1" -- q  - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
 vim.o.expandtab = true -- expand tab to spaces
 
 -- Backup files
 vim.o.backup = true -- use backup files
 vim.o.writebackup = false
 vim.o.swapfile = false -- do not use swap file
-vim.o.undodir = HOME .. '/.vim/tmp/undo//'     -- undo files
-vim.o.backupdir = HOME .. '/.vim/tmp/backup//' -- backups
-vim.o.directory = '/.vim/tmp/swap//'   -- swap files
+vim.o.undodir = HOME .. "/.vim/tmp/undo//" -- undo files
+vim.o.backupdir = HOME .. "/.vim/tmp/backup//" -- backups
+vim.o.directory = "/.vim/tmp/swap//" -- swap files
 
 vim.cmd([[
   au FileType python                  set ts=4 sw=4
@@ -70,7 +70,8 @@ vim.cmd([[
 
 -- Commands mode
 vim.o.wildmenu = true -- on TAB, complete options for system command
-vim.o.wildignore = 'deps,.svn,CVS,.git,.hg,*.o,*.a,*.class,*.mo,*.la,*.so,*.obj,*.swp,*.jpg,*.png,*.xpm,*.gif,.DS_Store,*.aux,*.out,*.toc'
+vim.o.wildignore =
+	"deps,.svn,CVS,.git,.hg,*.o,*.a,*.class,*.mo,*.la,*.so,*.obj,*.swp,*.jpg,*.png,*.xpm,*.gif,.DS_Store,*.aux,*.out,*.toc"
 
 -- Only show cursorline in the current window and in normal mode.
 vim.cmd([[
@@ -83,52 +84,49 @@ vim.cmd([[
   augroup END
 ]])
 
-
 -- Plugins
-require('indent_blankline').setup()
-require('toggleterm').setup({
-  open_mapping = [[<c-\>]],
+require("indent_blankline").setup()
+require("toggleterm").setup({
+	open_mapping = [[<c-\>]],
 })
-require('lualine').setup({
-  options = {
-    theme = 'catppuccin',
-  }
+require("lualine").setup({
+	options = {
+		theme = "catppuccin",
+	},
 })
 
-require('nvim-treesitter.configs').setup({
-  highlight = {
-    enable = true,
-  }
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true,
+	},
 })
 
 -- highlighting of word under cursor
-require('mini.cursorword').setup{}
+require("mini.cursorword").setup({})
 
 -- autopairing
-require('mini.pairs').setup{}
+require("mini.pairs").setup({})
 
-require('mini.map').setup{}
+require("mini.map").setup({})
 -- startup screen
-require('mini.starter').setup{}
-require('mini.trailspace').setup{}
+require("mini.trailspace").setup({})
 
-require('marks').setup({default_mappings = true})
+require("marks").setup({ default_mappings = true })
 
-require('workspaces').setup({
-  hooks = {
-    open = { "Telescope find_files" }
-  }
+require("workspaces").setup({
+	hooks = {
+		open = { "Telescope find_files" },
+	},
 })
-require('nvim-tree').setup{
-  diagnostics = {
-    enable = true,
-  }
-}
+require("nvim-tree").setup({
+	diagnostics = {
+		enable = true,
+	},
+})
 
 -- vim-test
-vim.g['test#strategy'] = 'neovim'
-vim.g['test#harpoon_term'] = 2
-
+vim.g["test#strategy"] = "neovim"
+vim.g["test#harpoon_term"] = 2
 
 vim.g.python3_host_prog = HOME .. "/.pyenv/versions/nvim/bin/python"
-vim.g.colorizer_auto_filetype='css,html,scss'
+vim.g.colorizer_auto_filetype = "css,html,scss"
