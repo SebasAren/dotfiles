bdff2b98d45029e02b9324eaab76f441d6d30aaa
Sebastiaan <sebastiaan@arendsen.dev>
2022-12-18 19:22:37 +0100

    Fixed wireplumber volume widget

--- qtile/.config/qtile/widgets/wireplumber.py
+++ qtile/.config/qtile/widgets/wireplumber.py
@@ -1,4 +1,5 @@
 import subprocess
+import shlex
 from libqtile.widget.volume import Volume
 
 
@@ -6,10 +7,22 @@ class WireplumberVolume(Volume):
     def get_volume(self):
         try:
             a = self.call_process(["wpctl", "get-volume", "@DEFAULT_AUDIO_SINK@"])
+        except subprocess.CalledProcessError:
+            return -1
+        try:
             if a.startswith("Volume: "):
                 volume = float(a[8:])
                 return volume * 100
             else:
                 return -1
-        except subprocess.CalledProcessError:
+        except ValueError:
             return -1
+
+    def cmd_mute(self):
+        subprocess.call(shlex.split("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"))
+
+    def cmd_decrease_vol(self):
+        subprocess.call(shlex.split("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"))
+
+    def cmd_increase_vol(self):
+        subprocess.call(shlex.split("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"))
