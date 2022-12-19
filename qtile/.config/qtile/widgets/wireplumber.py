514d74ae566ffd748431c215adc2f5d02c845fb4
Sebastiaan <sebastiaan@arendsen.dev>
2022-12-19 19:15:22 +0100

    Updated WireplumberVolume

--- qtile/.config/qtile/widgets/wireplumber.py
+++ qtile/.config/qtile/widgets/wireplumber.py
@@ -1,28 +1,30 @@
 import subprocess
 import shlex
 from libqtile.widget.volume import Volume
+from libqtile.widget import base
 
 
 class WireplumberVolume(Volume):
+    def __init__(self, **config):
+        base._TextBox.__init__(self, "0", **config)
+        self.add_defaults(Volume.defaults)
+        self.surfaces = {}
+        self.volume = None
+        self.add_callbacks(
+            {
+                "Button1": self.open_qpwgraph,
+            }
+        )
+
     def get_volume(self):
         try:
-            a = self.call_process(["wpctl", "get-volume", "@DEFAULT_AUDIO_SINK@"])
-        except subprocess.CalledProcessError:
-            return -1
-        try:
+            a = self.call_process(shlex.split("wpctl get-volume @DEFAULT_AUDIO_SINK@"))
             if a.startswith("Volume: "):
-                volume = float(a[8:])
-                return volume * 100
+                return float(a[8:]) * 100
             else:
                 return -1
-        except ValueError:
+        except (subprocess.CalledProcessError, ValueError):
             return -1
 
-    def cmd_mute(self):
-        subprocess.call(shlex.split("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"))
-
-    def cmd_decrease_vol(self):
-        subprocess.call(shlex.split("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"))
-
-    def cmd_increase_vol(self):
-        subprocess.call(shlex.split("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"))
+    def open_qpwgraph(self):
+        subprocess.Popen(shlex.split("qpwgraph"), shell=True)
