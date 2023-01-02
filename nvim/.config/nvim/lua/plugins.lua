79188cd36effecab6db0b71a629afc594662207b
Sebastiaan <sebastiaan@arendsen.dev>
2023-01-03 00:47:34 +0100

    Added neoclip

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -41,6 +41,16 @@ return require("packer").startup({
 				require("trouble").setup({})
 			end,
 		}) -- quickfix replacement
+		use({
+			"AckslD/nvim-neoclip.lua",
+			requires = {
+				{ "kkharji/sqlite.lua", module = "sqlite" },
+				{ "nvim-telescope/telescope.nvim" },
+			},
+			config = function()
+				require("neoclip").setup()
+			end,
+		})
 
 		-- git
 		use("rhysd/git-messenger.vim") -- see latest commit of line
--- nvim/.config/nvim/lua/telescope-config.lua
+++ nvim/.config/nvim/lua/telescope-config.lua
@@ -61,3 +61,4 @@ require("telescope").setup({
 })
 require("telescope").load_extension("fzf")
 require("telescope").load_extension("git_worktree")
+require("telescope").load_extension("neoclip")
