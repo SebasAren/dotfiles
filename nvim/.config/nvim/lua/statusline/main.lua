1391421962913b42ac3a0ad06499deec1f029214
Sebastiaan Arendsen <sebastiaan@tetra.nl>
2023-02-20 11:07:07 +0100

    nvim: added scrollbar and ruler to heirline

--- nvim/.config/nvim/lua/statusline/main.lua
+++ nvim/.config/nvim/lua/statusline/main.lua
@@ -3,6 +3,18 @@ local utils = require("heirline.utils")
 
 local Align = { provider = "%=" }
 local Space = { provider = " " }
+local Ruler = { provider = "%7(%l/%3L%):%2c %P" }
+local ScrollBar = {
+	static = {
+		sbar = { "🭶", "🭷", "🭸", "🭹", "🭺", "🭻" },
+	},
+	provider = function(self)
+		local curr_line = vim.api.nvim_win_get_cursor(0)[1]
+		local lines = vim.api.nvim_buf_line_count(0)
+		local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
+		return string.rep(self.sbar[i], 2)
+	end,
+}
 
 local StatusLine = {
 	require("statusline.vimode"),
@@ -11,6 +23,9 @@ local StatusLine = {
 	Space,
 	require("statusline.git"),
 	Align,
+	Ruler,
+	Space,
+	ScrollBar,
 }
 
 return {
