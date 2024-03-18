ddf39f3998290d0c7738e636e20262778e378cc3
Sebastiaan Arendsen <sebastiaan@tetra.nl>
2024-03-18 11:30:27 +0100

    added vue to emmet support

--- nvim/.config/nvim/lua/lsp-config.lua
+++ nvim/.config/nvim/lua/lsp-config.lua
@@ -134,6 +134,7 @@ lspconfig.emmet_language_server.setup({
 		"scss",
 		"pug",
 		"typescriptreact",
+		"vue",
 	},
 	-- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
 	-- **Note:** only the options listed in the table are supported.
