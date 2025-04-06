86f58d0e5c5091f34b01b240275e0efb33e540eb
Sebastiaan <sebastiaan@arendsen.dev>
2025-04-06 15:35:39 +0200

    nvim: auto show documentation on completion

--- nvim/.config/nvim/lua/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/plugins/autocomplete.lua
@@ -34,9 +34,11 @@ return {
 			nerd_font_variant = "mono",
 		},
 
-		-- (Default) Only show the documentation popup when manually triggered
+		-- Alway show documentation
 		completion = {
-			documentation = { auto_show = false },
+			documentation = {
+				auto_show = true,
+			},
 			trigger = {
 				show_in_snippet = false,
 			},
