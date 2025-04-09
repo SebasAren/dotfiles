a3e32fad66c4218e0ae22af5a433585375b32713
Sebastiaan <sebastiaan@arendsen.dev>
2025-04-09 17:49:11 +0200

    nvim: redid autocomplete mappings

--- nvim/.config/nvim/lua/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/plugins/autocomplete.lua
@@ -8,8 +8,35 @@ return {
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
-			keymap = { preset = "default" },
-			appearance = { nerd_font_variant = "mono" },
+			keymap = {
+				preset = "none",
+				["<C-space>"] = { "show", "show_documentation", "fallback" },
+				["<C-e>"] = { "cancel" },
+				["<Tab>"] = { "select_next", "fallback" },
+				["<C-n>"] = { "select_next", "fallback" },
+				["<S-Tab>"] = { "select_prev", "fallback" },
+				["<C-p>"] = { "select_prev", "fallback" },
+				["<C-y>"] = { "accept", "fallback" },
+				["<CR>"] = { "accept", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down" },
+				["<C-b>"] = { "scroll_documentation_up" },
+			},
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+			completion = {
+				list = {
+					selection = {
+						preselect = false,
+					},
+					cycle = {
+						from_top = false,
+					},
+				},
+				documentation = {
+					auto_show = true,
+				},
+			},
 			fuzzy = {
 				implementation = "prefer_rust_with_warning",
 			},
