9ca895cdbe9cd1d917972ab741c528236e7db71c
sebastiaan <sebastiaan@localhost.localdomain>
2025-04-10 14:17:46 +0200

    nvim: changed to deepseek r1

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -46,7 +46,7 @@ return {
 					__inherited_from = "openai",
 					endpoint = "https://openrouter.ai/api/v1",
 					api_key_name = "OPENROUTER_API_KEY",
-					model = "deepseek/deepseek-chat-v3-0324",
+					model = "deepseek/deepseek-r1",
 				},
 				openrouter_gemini = {
 					__inherited_from = "openai",
