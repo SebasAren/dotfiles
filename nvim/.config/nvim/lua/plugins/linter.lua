08d973e0aaf442dd265c07fabb2044ae10615d27
sebastiaan <sebastiaan@localhost.localdomain>
2025-05-08 13:49:25 +0200

    nvim: temporary disable eslint

--- nvim/.config/nvim/lua/plugins/linter.lua
+++ nvim/.config/nvim/lua/plugins/linter.lua
@@ -5,21 +5,21 @@ return {
 		local lint = require("lint")
 		lint.linters_by_ft = {
 			-- JavaScript/TypeScript and related filetypes use eslint_d
-			javascript = {
-				"eslint_d",
-			},
-			typescript = {
-				"eslint_d",
-			},
-			javascriptreact = {
-				"eslint_d",
-			},
-			typescriptreact = {
-				"eslint_d",
-			},
-			vue = {
-				"eslint_d",
-			},
+			-- javascript = {
+			-- 	"eslint_d",
+			-- },
+			-- typescript = {
+			-- 	"eslint_d",
+			-- },
+			-- javascriptreact = {
+			-- 	"eslint_d",
+			-- },
+			-- typescriptreact = {
+			-- 	"eslint_d",
+			-- },
+			-- vue = {
+			-- 	"eslint_d",
+			-- },
 			-- Python uses ruff for linting
 			python = {
 				"ruff",
