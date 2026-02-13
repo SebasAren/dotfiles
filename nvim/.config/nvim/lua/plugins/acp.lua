b9add6b0102e982cb4069a8d9141194822179434
Sebastiaan <sebastiaan@tetra.nl>
2026-02-13 21:28:46 +0100

    feat: switched from opencode.nvim to agentic.nvim

---
+++ nvim/.config/nvim/lua/plugins/acp.lua
@@ -0,0 +1,41 @@
+return {
+	"carlos-algms/agentic.nvim",
+	opts = {
+		provider = "opencode-acp",
+	},
+	keys = {
+		{
+			"<C-\\>",
+			function()
+				require("agentic").toggle()
+			end,
+			mode = { "n", "v", "i" },
+			desc = "Toggle Agentic Chat",
+		},
+		{
+			"<C-'>",
+			function()
+				require("agentic").add_selection_or_file_to_context()
+			end,
+			mode = { "n", "v" },
+			desc = "Add file or selection to Agentic to Context",
+		},
+		{
+			"<C-,>",
+			function()
+				require("agentic").new_session()
+			end,
+			mode = { "n", "v", "i" },
+			desc = "New Agentic Session",
+		},
+		{
+			"<A-i>r", -- ai Restore
+			function()
+				require("agentic").restore_session()
+			end,
+			desc = "Agentic Restore session",
+			silent = true,
+			mode = { "n", "v", "i" },
+		},
+	},
+}
