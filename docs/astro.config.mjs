2498670c0b121f50ffbce98bf71a3ce195abdbb6
Sebastiaan Arendsen <sebastiaan@arendsen.dev>
2026-04-25 02:34:16 +0200

    docs(astro): update site base path to match GitHub Pages repo

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   output: "static",
   site: "https://sebasaren.github.io",
-  base: "/dotfiles",
+  base: "/Sebbashop",
 });
